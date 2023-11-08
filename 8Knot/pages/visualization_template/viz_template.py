3531c264b7846d5b94d3364a94d5b21a30092a14
ahleexxxx <ahlee9896@gmail.com>
2023-11-08 17:22:35 -0600

    Update viz_template.py
    replace placeholder and insert actual values

--- 8Knot/pages/visualization_template/viz_template.py
+++ 8Knot/pages/visualization_template/viz_template.py
@@ -14,57 +14,23 @@ from cache_manager.cache_manager import CacheManager as cm
 from pages.utils.job_utils import nodata_graph
 import time
 
-"""
-NOTE: VARIABLES TO CHANGE:
-
-(1) PAGE
-(2) VIZ_ID
-(3) gc_VISUALIZATION
-(4) TITLE OF VISUALIZATION
-(5) CONTEXT OF GRAPH
-(6) IDs of Dash components
-(6) NAME_OF_VISUALIZATION_graph
-(7) COLUMN_WITH_DATETIME
-(8) COLUMN_WITH_DATETIME
-(9) COLUMN_TO_SORT_BY
-(10) Comments before callbacks
-(11) QUERY_USED, QUERY_NAME, QUERY_INITIALS
-
-NOTE: IMPORTING A VISUALIZATION INTO A PAGE
-(1) Include the visualization file in the visualization folder for the respective page
-(2) Import the visualization into the page_name.py file using "from .visualizations.visualization_file_name import gc_visualization_name"
-(3) Add the card into a column in a row on the page
-
-NOTE: ADDITIONAL DASH COMPONENTS FOR USER GRAPH CUSTOMIZATIONS
-
-If you add Dash components (ie dbc.Input, dbc.RadioItems, dcc.DatePickerRange...) the ids, html_for, and targets should be in the
-following format: f"component-identifier-{PAGE}-{VIZ_ID}"
-
-NOTE: If you change or add a new query, you need to do "docker system prune -af" before building again
-
-For more information, check out the new_vis_guidance.md
-"""
-
-
-# TODO: Remove unused imports and edit strings and variables in all CAPS
-# TODO: Remove comments specific for the template
-
-PAGE = "page-name"  # EDIT FOR CURRENT PAGE
-VIZ_ID = "shortname-of-viz"  # UNIQUE IDENTIFIER FOR VIZUALIZATION
+# Replace with actual values
+PAGE = "YourPageName"
+VIZ_ID = "YourVisualizationID"
 
 gc_VISUALIZATION = dbc.Card(
     [
         dbc.CardBody(
             [
                 html.H3(
-                    "TITLE OF VISUALIZATION",
+                    "Your Visualization Title",
                     className="card-title",
                     style={"textAlign": "center"},
                 ),
                 dbc.Popover(
                     [
                         dbc.PopoverHeader("Graph Info:"),
-                        dbc.PopoverBody("INSERT CONTEXT OF GRAPH HERE"),
+                        dbc.PopoverBody("Provide a brief description of your graph here."),
                     ],
                     id=f"popover-{PAGE}-{VIZ_ID}",
                     target=f"popover-target-{PAGE}-{VIZ_ID}",
@@ -91,8 +57,7 @@ gc_VISUALIZATION = dbc.Card(
                                                 {
                                                     "label": "Trend",
                                                     "value": "D",
-                                                },  # TREND IF LINE, DAY IF NOT
-                                                # {"label": "Week","value": "W",}, UNCOMMENT IF APPLICABLE
+                                                },
                                                 {"label": "Month", "value": "M"},
                                                 {"label": "Year", "value": "Y"},
                                             ],
@@ -114,46 +79,7 @@ gc_VISUALIZATION = dbc.Card(
                             ],
                             align="center",
                         ),
-                        # TODO: ADD IN IF ADDITIONAL PARAMETERS FOR GRAPH, REMOVE IF NOT
-                        """ format dbc.Inputs, including dbc.Alert if needed
-                                dbc.Label(
-                                    "TITLE_OF_ADDITIONAL_PARAMETER:",
-                                    html_for=f"component-identifier-{PAGE}-{VIZ_ID}",
-                                    width={"size": "auto"},
-                                ),
-                                dbc.Col(
-                                    dbc.Input(
-                                        id=f"component-identifier-{PAGE}-{VIZ_ID}",,
-                                        type="number",
-                                        min=1,
-                                        max=120,
-                                        step=1,
-                                        value=7,
-                                    ),
-                                    className="me-2",
-                                    width=2,
-                                ),
-                                dbc.Alert(
-                                    children="Please ensure that 'PARAMETER' is less than 'PARAMETER'",
-                                    id=f"component-identifier-{PAGE}-{VIZ_ID}",
-                                    dismissable=True,
-                                    fade=False,
-                                    is_open=False,
-                                    color="warning",
-                                ),
-                        """
-                        """ format for dcc.DatePickerRange:
-                                dbc.Col(
-                                    dcc.DatePickerRange(
-                                        id=f"component-identifier-{PAGE}-{VIZ_ID}",
-                                        min_date_allowed=dt.date(2005, 1, 1),
-                                        max_date_allowed=dt.date.today(),
-                                        clearable=True,
-                                    ),
-                                    width="auto",
-                                ),
-
-                        """,
+                        # You can add additional components here
                     ]
                 ),
             ]
@@ -161,8 +87,7 @@ gc_VISUALIZATION = dbc.Card(
     ],
 )
 
-
-# callback for graph info popover
+# Callback for graph info popover
 @callback(
     Output(f"popover-{PAGE}-{VIZ_ID}", "is_open"),
     [Input(f"popover-target-{PAGE}-{VIZ_ID}", "n_clicks")],
@@ -173,21 +98,18 @@ def toggle_popover(n, is_open):
         return not is_open
     return is_open
 
-
-# callback for VIZ TITLE graph
+# Callback for your visualization graph
 @callback(
     Output(f"{PAGE}-{VIZ_ID}", "figure"),
-    # Output(f"check-alert-{PAGE}-{VIZ_ID}", "is_open"), USE WITH ADDITIONAL PARAMETERS
-    # if additional output is added, change returns accordingly
     [
         Input("repo-choices", "data"),
         Input(f"date-radio-{PAGE}-{VIZ_ID}", "value"),
-        # add additional inputs here
+        # Add additional inputs here as needed
     ],
     background=True,
 )
-def NAME_OF_VISUALIZATION_graph(repolist, interval):
-    # wait for data to asynchronously download and become available.
+def your_visualization_graph(repolist, interval):
+    # Wait for data to asynchronously download and become available
     cache = cm()
     df = cache.grabm(func=QUERY_INITIALS, repos=repolist)
     while df is None:
@@ -197,44 +119,27 @@ def NAME_OF_VISUALIZATION_graph(repolist, interval):
     start = time.perf_counter()
     logging.warning(f"{VIZ_ID}- START")
 
-    # test if there is data
+    # Test if there is data
     if df.empty:
         logging.warning(f"{VIZ_ID} - NO DATA AVAILABLE")
         return nodata_graph
 
-    # function for all data pre processing, COULD HAVE ADDITIONAL INPUTS AND OUTPUTS
+    # Implement your custom data processing logic here
     df = process_data(df, interval)
 
+    # Generate the Plotly figure for your graph
     fig = create_figure(df, interval)
 
     logging.warning(f"{VIZ_ID} - END - {time.perf_counter() - start}")
     return fig
 
-
 def process_data(df: pd.DataFrame, interval):
-    """Implement your custom data-processing logic in this function.
-    The output of this function is the data you intend to create a visualization with,
-    requiring no further processing."""
-
-    # convert to datetime objects rather than strings
-    # ADD ANY OTHER COLUMNS WITH DATETIME
-    df["COLUMN_WITH_DATETIME"] = pd.to_datetime(df["COLUMN_WITH_DATETIME"], utc=True)
-
-    # order values chronologically by COLUMN_TO_SORT_BY date
-    df = df.sort_values(by="COLUMN_TO_SORT_BY", axis=0, ascending=True)
-
-    """LOOK AT OTHER VISUALIZATIONS TO SEE IF ANY HAVE A SIMILAR DATA PROCESS"""
-
+    # Implement your custom data processing logic here
+    # Convert to datetime objects, sort, and preprocess data
+    # Replace these comments with your actual code
     return df
 
-
 def create_figure(df: pd.DataFrame, interval):
-    # time values for graph
-    x_r, x_name, hover, period = get_graph_time_values(interval)
-
-    # graph generation
-    fig = fig
-
-    """LOOK AT OTHER VISUALIZATIONS TO SEE IF ANY HAVE A SIMILAR GRAPH"""
-
+    # Implement your graph generation logic here
+    # Replace these comments with your actual code
     return fig
